da4466a463ceaf5d2d6253f3d08988e432c09191
Karan Patel <karanj707mern@gmail.com>
2025-11-11 09:59:50 +0530

    working on jwt

--- Backend/Model/catModel.js
+++ Backend/Model/catModel.js
@@ -1,12 +1,7 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-mongoose.connect(process.env.MONGO_URI, {
-  
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  serverSelectionTimeoutMS: 5000, // 5 seconds timeout
-})
+mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('Connected!'))
   .catch(err => console.error('Connection error', err));
 
---
+++ Backend/Model/userModel.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log('Connected!'))
+require('dotenv').config();
+
+const schema = mongoose.Schema;
+const userSchema = new schema ({
+    username: String,
+    email: String ,
+    password:String
+});
+const userModel = mongoose.model("user",userSchema);
+module.exports = userModel
