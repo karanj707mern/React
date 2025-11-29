7ba8e13f2b94ede8b022baa1d8be6c889cedcb18
Karan Patel <karanj707mern@gmail.com>
2025-11-29 16:24:20 +0530

    Fix file/folder casing for Linux deployment

--- Backend/Routes/catRoutes.js
+++ Backend/Routes/catRoutes.js
@@ -1,4 +1,4 @@
-var {disp,ins,upd,edit}  = require("../controllers/catcontroller")
+var {disp,ins,upd,edit}  = require("../controllers/catController")
 var express = require('express')
 var router = express.Router()
 const auth = require('../middleware/auth')
